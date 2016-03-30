ad246120a73fb1015eb60f63165a37fa923e297a
Keith <gsh_1234567@126.com>
2016-03-29 17:54:58 -0700

    Initial upload

---
+++ app/src/main/java/com/keith/databindingdemo/model/User.java
@@ -0,0 +1,20 @@
+package com.keith.databindingdemo.model;
+
+/**
+ * Created by Keith on 3/29/2016.
+ */
+public class User {
+    private final String firstName;
+    private final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
